9ead2d144716274b41f484a7dd679952b711fc05
aakashrstg00 <1998aakashrastogi@gmail.com>
2017-03-26 01:05:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var urls = {
-    currentWeatherURL: 'http://api.openweathermap.org/data/2.5/weather?q='
+    currentWeatherURL: 'https://api.openweathermap.org/data/2.5/weather?q='
     , apikey: '857d06912a6bba329cc9256d13fc8890'
 };
